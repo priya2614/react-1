0bae7416d333d4051c79fc1cc0b7b0c4db933e95
priya2614 <165402467+priya2614@users.noreply.github.com>
2024-05-20 15:49:44 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,57 @@
+
+import "./App.css"
+
+const App = () =>{
+
+    return(
+        <>
+        <h2 id="header">E-Commerce</h2>
+
+        <form id="container">
+            <h2 id="head2"><i>Please Signup...!</i></h2>
+            <input type="text" placeholder="Enter Your First Name"/>
+            <input type="text" placeholder="Enter Your Last Name"/>
+            <input type="email" placeholder="Enter Your Email ID"/>
+            <input type="passward" placeholder="Enter Your Passward"/>
+            <button id="btn"><b>Create..!</b></button>
+        </form>
+        <div className="flex">
+            <img className="image" src="https://www.foodiesfeed.com/wp-content/uploads/2023/06/burger-with-melted-cheese.jpg" alt="Image"/>
+            <h3 className="head3"><i>Burger</i> </h3>
+            <p className="para">A burger is a patty of ground beef grilled and placed between two halves of a bun. Slices of raw onion, lettuce, bacon, mayonnaise, and other ingredients add flavor. Burgers are considered an American food but are popular around the world. In Japan, teriyaki burgers are popular. </p>
+
+            <img className="image" src="https://cdn.pixabay.com/photo/2020/05/17/04/22/pizza-5179939_640.jpg" alt="Image"/>
+            <h3 className="head3"><i>Pizza</i> </h3>
+            <p className="para">pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot  </p>
+
+            <img className="image" src="https://img.freepik.com/free-photo/gourmet-chicken-biryani-with-steamed-basmati-rice-generated-by-ai_188544-13480.jpg" alt="Image"/>
+            <h3 className="head3"><i>Biriyani</i> </h3>
+            <p className="para">It is made with rice, some type of meat (chicken, goat, pork, lamb, beef, prawn, or fish) and spices. To cater to vegetarians, in some cases, it is prepared by substituting vegetables or paneer for the meat. </p>
+
+            <img className="image" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqNLyGryhHF6GubTuCgTrD4-Dks2o8F7HPz-vC16he0w&s" alt="Image"/>
+            <h3 className="head3"><i>Noodles</i> </h3>
+            <p className="para">Noodles are a type of food made from unleavened dough which is either rolled flat and cut, stretched, or extruded, into long strips or strings. </p>
+
+            <img className="image" src="https://www.allrecipes.com/thmb/SoBuPU73KcbYHl3Kp3j8Xx4A3fc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/8805-CrispyFriedChicken-mfs-3x2-072-d55b8406d4ae45709fcdeb58a04143c2.jpg" alt="Image"/>
+            <h3 className="head3"><i>Chicken Fry</i> </h3>
+            <p className="para">Fried chicken, also known as Southern fried chicken, is a dish consisting of chicken pieces that have been coated with seasoned flour or batter </p>
+
+            <img className="image" src="https://nishkitchen.com/wp-content/uploads/2015/04/Chettinad-fish-fry-1B.jpg" alt="Image"/>
+            <h3 className="head3"><i>Fish Fry</i> </h3>
+            <p className="para">A fish fry is a meal containing battered or breaded fried fish. It usually also includes french fries, coleslaw, macaroni salad, lemon slices, tartar sauce, hot sauce, malt vinegar and dessert. Some Native American versions are cooked by coating fish with semolina and egg yolk. </p>
+
+            <img className="image" src="https://www.kitchensanctuary.com/wp-content/uploads/2020/04/Chicken-Fried-Rice-square-FS-.jpg" alt="Image"/>
+            <h3 className="head3"><i>Fried Rice</i> </h3>
+            <p className="para">Fried rice is a dish of cooked rice that has been stir-fried in a wok or a frying pan and is usually mixed with other ingredients such as eggs, vegetables, seafood, or meat. It is often eaten by itself or as an accompaniment to another dish. </p>
+
+            <img className="image" src="https://thumbs.dreamstime.com/b/soft-drinks-11566390.jpg" alt="Image"/>
+            <h3 className="head3"><i>Soft Drinks</i> </h3>
+            <p className="para">soft drink, any of a class of nonalcoholic beverages, usually but not necessarily carbonated, normally containing a natural or artificial sweetening agent, edible acids, natural or artificial flavours, and sometimes juice. </p>
+
+        </div>
+        
+        </>
+    )
+}
+
+export default App
---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDom from 'react-dom/client';
+import MyApp from './App.jsx'
+
+ReactDom.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+  <MyApp/>
+ </React.StrictMode>
+)
